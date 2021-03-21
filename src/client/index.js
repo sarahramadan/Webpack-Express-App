5c57af4b359e79e00c969b5bd2f1af6103b11d54
Sarah <engsarah2014@gmail.com>
2021-03-21 20:00:18 +0200

    convert css to scss

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,13 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+
+
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
 console.log(checkForName);
 console.log('sarah');
 alert("I EXIST")
